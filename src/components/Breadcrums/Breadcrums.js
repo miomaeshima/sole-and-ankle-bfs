e2f0fdf931771e3fec5acc8dabc2b1882ef7107f
miomaeshima <miomamomo@gmail.com>
2021-05-16 16:26:03 +0900

    edited navigation component

---
+++ src/components/Breadcrums/Breadcrums.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import {COLORS, WEIHGT} from '../../constants';
+
+const Breadcrumbs = () =>{
+    return (
+        <>Bredcrumbs</>
+    )
+    
+}
+
+
+export default Breadcrumbs;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import { COLORS, WEIGHT } from "../../constants";
+import Breadcrumbs from "../Breadcrums";
 
-
-const Nav = () =>{
-    return (
-        <Wrapper>
-        Nav
-        </Wrapper>
-
-    )
-}
+const Nav = () => {
+  return (
+    <Wrapper>
+      <Breadcrumbs />
+      <ol>
+        <Li>Lifestyle</Li>
+        <Li>jordan</Li>
+        <Li>running</Li>
+        <Li>basketball</Li>
+        <Li>training & gym</Li>
+        <Li>football</Li>
+        <Li>american football</Li>
+        <Li>baseball</Li>
+        <Li>golf</Li>
+        <Li>tennis</Li>
+        <Li>athletics</Li>
+        <Li>walking</Li>
+      </ol>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
-    flex: 0 1 250px;
-    background: lightgreen;
+  flex: 0 1 250px;
+  background: lightgreen;
+  padding: 0 32px;
 `;
 
+const Li = styled.li`
+ text-transform: capitalize;
+ font-size: 16px;
+ line-height: 32px;
+`;
 export default Nav;
